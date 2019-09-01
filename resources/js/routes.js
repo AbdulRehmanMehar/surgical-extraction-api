b24d0ec333635e2831d3f52404807878c6eba56b
Abdul Rehman <mehars.6925@gmail.com>
2019-09-01 06:42:41 +0500

    updated

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,10 +1,29 @@
-import Home from './views/Home'
+const Home = () => import('./views/Home')
+const Login = () => import('./views/auth/Login')
+const Register =  () => import('./views/auth/Register')
+const ResetPassword = () => import('./views/auth/Reset')
 
 const routes = [
     {
         path: '/',
+        name: 'home',
         component: Home
-    }
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: Register
+    },
+    {
+        path: '/reset-password',
+        name: 'reset-password',
+        component: ResetPassword
+    },
 ]
 
 export default routes
