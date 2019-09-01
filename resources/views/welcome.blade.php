314e438fc5bcd14825e8e5236c67c9b81f0c1804
Abdul Rehman <mehars.6925@gmail.com>
2019-09-01 06:44:44 +0500

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,5 +11,9 @@
     <body>
         <div id="vue-application"></div>
         <script src="{{ asset('js/app.js') }}"></script>
+        <link rel="stylesheet" 
+        href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" 
+        integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" 
+        crossorigin="anonymous">
     </body>
 </html>
