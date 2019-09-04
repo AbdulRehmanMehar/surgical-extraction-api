3c9e28d212aca06d253a8eca283f67aa7f4d9983
Abdul Rehman <mehars.6925@gmail.com>
2019-09-04 22:55:29 +0500

    added resources for controllers

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,21 @@ use Illuminate\Http\Request;
 Route::post('/login', 'AuthController@login');
 Route::post('/logout', 'AuthController@logout');
 Route::post('/register', 'AuthController@register');
+Route::post('/reset-password', 'AuthController@reset_password');
+Route::post('/verify-email/{id}', 'AuthController@verify_email');
+Route::get('/reset-password/{id}', 'AuthController@reset_password');
+Route::post('/delete-account/{id}', 'AuthController@delete_account');
+Route::get('/cancel-reset-password/{id}', 'AuthController@cancel_reset_request');
 
+
+Route::group(['middleware' => 'jwtauth'], function () {
+    Route::get('/protected', function() {
+        return response()->json(['message' => 'worked'], 200);
+    });
+    Route::resource('address', 'AddressController', [
+        'except' => ['edit', 'create']
+    ]);
+    Route::resource('category', 'CategoryController', [
+        'except' => ['edit', 'create']
+    ]);
+});
