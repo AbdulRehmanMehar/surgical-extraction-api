7861c3d58333a68895b9a3472b18bdbad60776ba
Abdul Rehman <mehars.6925@gmail.com>
2019-09-04 22:49:48 +0500

    product model

---
+++ app/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name', 'price', 'description', 'category'
+    ];
+
+    public function category() {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function images() {
+        return $this->morphMany('App\Image', 'imageable');
+    }
+}
