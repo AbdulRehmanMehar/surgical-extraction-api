e28d669e07afb5ad431dedf826c07506268c221c
Abdul Rehman <mehars.6925@gmail.com>
2019-09-06 13:45:55 +0500

    fix bug

--- app/User.php
+++ app/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable implements JWTSubject
     }
 
     public function image() {
-        return $this->morph('App\Image', 'imageable');
+        return $this->morphMany('App\Image', 'imageable');
     }
 
     public function address() {
