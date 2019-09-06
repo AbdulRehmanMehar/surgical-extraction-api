6238dd991e9b726481bd44de6ff24bf75d202427
Abdul Rehman <mehars.6925@gmail.com>
2019-09-06 13:41:36 +0500

    User resource file

---
+++ app/Http/Resources/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\Image as ImageResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\Address as AddressResource;
+
+class User extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'verified' => $this->email_verified_at != null,
+            'images' => ImageResource::collection($this->image),
+            'address' => AddressResource::collection($this->address)
+        ];
+    }
+}
