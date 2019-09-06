722a81f6257a78a1b1172d4263b0054c630aef77
Abdul Rehman <mehars.6925@gmail.com>
2019-09-06 08:17:49 +0500

    added fillable attribute

--- app/Image.php
+++ app/Image.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+
+    protected $fillable = [
+        'data', 'imageable_id', 'imageable_type'
+    ];
+
     public function imageable() {
         return $this->morphTo();
     }
