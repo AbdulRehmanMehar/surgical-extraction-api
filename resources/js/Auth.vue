d4739e493364792719baab568e8a069cb6931e77
Abdul Rehman <mehars.6925@gmail.com>
2019-09-11 13:30:56 +0500

    wrapper for protected routes

---
+++ resources/js/Auth.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    beforeRouteEnter(to, from, next) {
+        next(vm => {
+            if (!vm.$root.isLoggedIn) {
+                vm.$toastr('danger', 'You\'re not logged in.', 'Aaah!')
+                vm.$router.go(-1)
+            }
+        })
+    }
+}
+</script>
