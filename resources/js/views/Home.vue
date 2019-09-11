6eb7dfa3fca67af1e4508653e38b9cdc9f23dc20
Abdul Rehman <mehars.6925@gmail.com>
2019-09-11 13:37:10 +0500

    loads featured products from server

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,11 +1,45 @@
 <template>
     <div id="home">
-        Hey Home
+        <Slideshow v-if="featured_products && featured_products.length" :products="featured_products" />
+        <div class="container" style="margin: 50px auto;">
+            <div class="columns is-multiline">
+                <div class="column is-4">
+                    <Product />
+                </div>
+                <div class="column is-4">
+                    <Product />
+                </div>
+                <div class="column is-4">
+                    <Product />
+                </div>
+                <div class="column is-4">
+                    <Product />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+const Product = () => import('../components/Product')
+const Slideshow = () => import('../components/Slideshow')
 export default {
-    name: 'home'
+    name: 'home',
+    components: {
+        Product,
+        Slideshow
+    },
+    data() {
+        return {
+            featured_products: null
+        }
+    },
+    created() {
+        this.$store.dispatch('get_featured_products')
+        .then(resp => {
+            this.featured_products = resp.data.data;
+        }).catch(error => error)
+    }
+
 }
 </script>
