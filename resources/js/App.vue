431dd7523d5a11cec2bc85c3101709c225a540c0
Abdul Rehman <mehars.6925@gmail.com>
2019-09-11 13:30:25 +0500

    updated

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,20 +1,35 @@
 <template>
     <div id="app">
-        <nprogress-container></nprogress-container>
-        <Navigation v-if="$root.navbar" />
-        <router-view></router-view>
+        <Preloader :class="{'hidden': !$root.loading}" />
+        <div>
+            <Navigation v-if="$root.navbar" />
+            <router-view></router-view>
+            <Foot v-if="$root.footer" />
+        </div>
     </div>
 </template>
 
 <script>
+const Foot = () => import('./components/Foot')
+const Preloader = () => import('./components/Preloader')
 const Navigation = () => import('./components/Navigation')
-const NprogressContainer = () => import('vue-nprogress/src/NprogressContainer')
 
 export default {
     name: 'app',
+    created: function () {
+        this.$http.interceptors.response.use(undefined, function (err) {
+        return new Promise(function (resolve, reject) {
+            if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+            this.$store.dispatch(logout)
+            }
+            throw err;
+        });
+        });
+    },
     components: {
+        Foot,
+        Preloader,
         Navigation,
-        NprogressContainer
     }
 
 }
@@ -27,5 +42,12 @@ export default {
     --app-accent-color: #589694;
     --app-text-hover-color: #FF8552;
     --app-text-accent-color: #E6E6E6;
+}
+.columns {
+    margin-left: 0px;
+    margin-right: 0px;
+}
+.hidden {
+    display: none;
 }
 </style>
