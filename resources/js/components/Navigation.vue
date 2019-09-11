3e5961c23f46535b1a0bd6ca913c573692805808
Abdul Rehman <mehars.6925@gmail.com>
2019-09-11 13:34:24 +0500

    updated

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,9 +1,9 @@
 <template>
-    <nav class="navbar">
+    <nav class="navbar is-fixed-top">
         <div class="container is-fluid">
             <div class="navbar-brand">
                 <router-link class="navbar-item" :to="{name: 'home'}">
-                    eSolution
+                    {{ $root.name }}
                 </router-link>
                 <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
                     <span></span>
@@ -13,10 +13,22 @@
             </div>
 
             <div id="navbarExampleTransparentExample" class="navbar-menu">
-                
-                <div class="navbar-end">
-                    <router-link :to="{name: 'login'}" class="navbar-item">Login</router-link>                
-                    <router-link :to="{name: 'register'}" class="navbar-item">Register</router-link>                
+
+                <div class="navbar-end" v-if="$root.isLoggedIn">
+                    <nav class="navbar" role="navigation" aria-label="dropdown navigation">
+                        <div class="navbar-item has-dropdown is-hoverable">
+                            <a @click.prevent="function(){}" class="navbar-link">{{ $root.currentUser.name }}</a>
+                            <div class="navbar-dropdown">
+                                <router-link :to="{name: 'dashboard'}" class="navbar-item">Dashboard</router-link>
+                                <router-link :to="{name: 'logout'}" class="navbar-item">Logout</router-link>
+                                <!-- <a @click.prevent="$root.logout" class="navbar-item">Logout</a> -->
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+                <div class="navbar-end" v-else>
+                    <router-link :to="{name: 'login'}" class="navbar-item">Login</router-link>
+                    <router-link :to="{name: 'register'}" class="navbar-item">Register</router-link>
                 </div>
             </div>
         </div>
@@ -25,9 +37,7 @@
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+
     }
 </script>
 
@@ -39,9 +49,11 @@
     transition: all 320ms;
     color: var(--app-text-color);
 }
-.navbar-item:hover, .navbar-link:hover, .navbar-item:focus, .navbar-link:focus, .router-link-exact-active  {
+.navbar-item:hover, .navbar-link:hover, .navbar-item:focus, .navbar-link:focus, .router-link-exact-active, .navbar-item.has-dropdown:hover, .navbar-item.has-dropdown:focus  {
     transition: all 320ms;
     color: var(--app-text-accent-color);
     background-color: var(--app-accent-color);
 }
+
+
 </style>
