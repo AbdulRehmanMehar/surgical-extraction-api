cda054718c46c1cc1d358ff0107b88ba89226346
Abdul Rehman <mehars.6925@gmail.com>
2019-09-15 15:58:49 +0500

    updated

--- resources/js/vuex/index.js
+++ resources/js/vuex/index.js
@@ -1,9 +1,11 @@
 import auth from './modules/auth'
 import product from './modules/product'
+import category from './modules/category'
 
 export default {
     modules: {
         auth,
-        product
+        product,
+        category
     }
 }
