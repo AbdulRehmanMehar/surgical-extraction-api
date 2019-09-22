f0e7fb2d4098a225be5a5a7898840713fd6351bc
Abdul Rehman <mehars.6925@gmail.com>
2019-09-23 00:57:06 +0500

    updated layout

--- resources/js/components/Product.vue
+++ resources/js/components/Product.vue
@@ -1,29 +1,28 @@
 <template>
     <div id="product">
         <div class="card" v-if="product">
-            <carousel :perPage="1" :autoplay="true" :loop="true" :paginationEnabled="false" class="card-image">
-                <div class="image-wrapper" v-if="product.images && product.images.length">
-                    <slide  v-for="image in product.images" :key="image.id" class="image is-5by3" :style="{'background-image': 'url(data:image/png;base64,' + image.image + ')' }">
-                    </slide>
-                </div>
-                <div class="image-wrapper" v-else>
-                    <slide class="image is-5by3" :style="{'background-image': 'url(' + dummyImage + ')' }">
-                    </slide>
-                </div>
+            <carousel :perPage="1" :autoplay="true" :loop="true" :paginationEnabled="false" class="card-image" v-if="product.images && product.images.length">
+                <slide  v-for="image in product.images" :key="image.id" class="image is-5by3" :style="{'background-image': 'url(data:image/png;base64,' + image.data + ')' }">
+                </slide>
             </carousel>
+            <div v-else>
+                <div class="image is-5by3" :style="{'background-image': 'url(' + dummyImage + ')' }">
+                </div>
+            </div>
             <div class="card-content">
                 <div class="content">
                     <h1 class="title is-5">{{ product.name }}</h1>
                     <h6 class="subtitle is-6">@<router-link :to="{name: 'category', params: {id: product.category.id}}">{{product.category.name}}</router-link></h6>
                     <!-- <p class="subtitle is-6">{{ product.description.slice(0, 200) + '...    ' }}</p> -->
                 </div>
-                <div class="field is-grouped">
-                    <div class="control">
-                        <router-link :to="{name: 'product', params: {id: product.id}}" class="button is-link is-rounded is-fullwidth">Details</router-link>
-                    </div>
-                    <div class="control">
-                        <a @click.prevent="$root.addToCart($event, product.id)" class="button is-success is-rounded is-fullwidth">Add to Cart</a>
-                    </div>
+                <div>
+                    <!-- <div class="control"> -->
+                        <router-link :to="{name: 'product', params: {id: product.id}}" class="button is-link is-rounded is-fullwidth" style="margin-bottom: 10px;">Details</router-link>
+                    <!-- </div> -->
+                    <!-- <div class="control"> -->
+                        <a v-if="$root.cart.find(x => x.product.id == product.id)" @click.prevent="$root.removeFromCart($event, {product})" class="button is-danger is-rounded is-fullwidth">Remove From Cart</a>
+                        <a v-else @click.prevent="$root.addToCart($event, product)" class="button is-success is-rounded is-fullwidth">Add to Cart</a>
+                    <!-- </div> -->
                 </div>
             </div>
         </div>
