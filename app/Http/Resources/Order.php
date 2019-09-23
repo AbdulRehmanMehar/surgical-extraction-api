d118a6fc6ca38716c1bf94ebbe04154b90c5cce5
Abdul Rehman <mehars.6925@gmail.com>
2019-09-24 00:42:01 +0500

    order api resource

---
+++ app/Http/Resources/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Order extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'cart' => $this->cart,
+            'status' => $this->status,
+            'details' => $this->details,
+            'user' => $this->user,
+            'address' => $this->user->address
+        ];
+    }
+}
