05239267fe1595805994bfdb22745631d7b2f58d
Abdul Rehman <mehars.6925@gmail.com>
2019-09-24 17:14:13 +0500

    added cors

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh\Cors\ServiceProvider::class,
         Sichikawa\LaravelSendgridDriver\SendgridTransportServiceProvider::class,
 
         /*
