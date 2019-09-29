bcf77287c05198423c81322011ad9360ac1c2ee8
Abdul Rehman <mehars.6925@gmail.com>
2019-09-29 21:36:55 +0500

    redirects to frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,13 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ env('APP_NAME') }}</title>
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <div id="vue-application"></div>
-        <script src="{{ asset('js/app.js') }}"></script>
-        <link rel="stylesheet" 
-        href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" 
-        integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" 
-        crossorigin="anonymous">
+        Redirecting you to the website.....
+        <script>
+            setTimeout(() => {
+                window.location.replace('{{ env("FRONTEND_URL") }}')
+            }, 4000);
+        </script>
     </body>
 </html>
